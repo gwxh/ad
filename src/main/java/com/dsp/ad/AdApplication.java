61c660d1f1bc48361ea2c34d972ad00a10762b18
wanghh <405955476@qq.com>
2018-05-27 22:06:02 +0800

    过滤器配置

--- src/main/java/com/dsp/ad/AdApplication.java
+++ src/main/java/com/dsp/ad/AdApplication.java
@@ -2,8 +2,10 @@ package com.dsp.ad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class AdApplication {
 
     public static void main(String[] args) {
