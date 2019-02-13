3c81ff96c97a74cc8420f2b5e3337ac937d094b9
wanghh <405955476@qq.com>
2019-02-13 10:35:00 +0800

    界面优化

--- src/main/java/com/dsp/ad/entity/subentity/PlanParam.java
+++ src/main/java/com/dsp/ad/entity/subentity/PlanParam.java
@@ -1,19 +1,115 @@
 package com.dsp.ad.entity.subentity;
 
+import com.dsp.ad.config.C;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 
 import java.io.IOException;
 
+import static com.dsp.ad.config.C.OBJECT_MAPPER;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PlanParam {
 
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final Logger LOGGER = LoggerFactory.getLogger(PlanParam.class);
+
+    private int platform;  //平台 0.移动端 1.PC
+
+    public int getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(int platform) {
+        this.platform = platform;
+    }
+
+    private int system; //系统 0.Android 1.IOS
+
+    public int getSystem() {
+        return system;
+    }
+
+    public void setSystem(int system) {
+        this.system = system;
+    }
+
+    private int style; //广告样式
+
+    public int getStyle() {
+        return style;
+    }
+
+    public void setStyle(int style) {
+        this.style = style;
+    }
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private String image;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    /**
+     * 传参需要
+     */
+    private MultipartFile imageFile;
+
+    public MultipartFile getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(MultipartFile imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    private int sex;
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    private String channel;
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
 
     /**
      * 投放速率
@@ -72,7 +168,7 @@ public class PlanParam {
 
     public String toJson() {
         try {
-            return OBJECT_MAPPER.writeValueAsString(this);
+            return C.OBJECT_MAPPER.writeValueAsString(this);
         } catch (JsonProcessingException e) {
             LOGGER.error(e.getMessage(), e);
         }
